716b51941a2f723deb1b47856a67bf48365dbb8a
JiaminYuan <ymimi1121@gmail.com>
2022-09-08 09:04:47 -0400

    html

---
+++ app/views/Main/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Site Landing</title>
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<p>Welcome to this new application</p>
+	<ul>
+		<li><a href='/Main/index'>Landing Page</a></li>
+		<li><a href='/Main/about_us'>About Us</a></li>
+		<li><a href='/Contact/index'>Contact Us</a></li>
+		<li><a href='/Contact/read'>See the messages we get</a></li>
+	</ul>
+</body>
+</html>
